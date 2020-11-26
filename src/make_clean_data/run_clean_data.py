1fcc8098a030898422ffbd34b7d0f14ce33626ff
NadejdaJ <nadejda@kth.se>
2020-11-26 21:22:47 +0000

    Exploring data and identifying closed shops and outdated products

--- src/make_clean_data/run_clean_data.py
+++ src/make_clean_data/run_clean_data.py
@@ -32,15 +32,30 @@ train_wo_outliers = remove_outliers(raw_train)
 item_categories = group_item_categories(raw_item_categories)
 items = update_items_with_new_categories(raw_items, item_categories)
 
-# Add rare tag to items
-items = items.append(pd.DataFrame({COLUMN_NAMES["item_name"]: ["Rare"],
-                                   COLUMN_NAMES["item_id"]: []}))
+# Add outdated tag to items
+item_sales_overtime = \
+    train_wo_outliers[train_wo_outliers["item_cnt_day"] >
+                      0].groupby(["date_block_num", "item_id"])["item_cnt_day"].sum().reset_index()
+print(item_sales_overtime)
 
-rare_df = train_wo_outliers.groupby("item_id")["date_block_num"].count().\
-    reset_index().rename(columns={"date_block_num": "count"})
-rare_df["count_perc"] = rare_df["count"] / len(train_wo_outliers.index) * 100
+# If no item sales in the last 6 months then mark item as "outdated"
+sold = item_sales_overtime[item_sales_overtime["date_block_num"] >= 27]["item_id"].unique()
 
-rare_df["tag"] = rare_df["count_perc"] < rare_cutoff
-print(rare_df)
-print(rare_df["tag"].sum())
-print(rare_df.describe())
+train_wo_outliers["outdated"] = False
+train_wo_outliers.loc[~train_wo_outliers["item_id"].isin(sold), "outdated"] = True
+print(train_wo_outliers)
+print(train_wo_outliers.describe())
+
+# If no sales in shop in the last 6 months then mark shop as "closed"
+open_shops = \
+    train_wo_outliers[train_wo_outliers["item_cnt_day"] >
+                      0].groupby(["date_block_num", "shop_id"])["item_cnt_day"].sum().reset_index()
+
+open_shops = open_shops[open_shops["date_block_num"] >= 27]["shop_id"].unique()
+print(len(open_shops))
+
+train_wo_outliers["shop_open"] = False
+train_wo_outliers.loc[train_wo_outliers["shop_id"].isin(open_shops), "shop_open"] = True
+print(train_wo_outliers)
+
+# Combine
